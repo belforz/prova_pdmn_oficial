ae6c7152129012c19a8e51dc1c10b4aa9f86144c
belforz <macedobeiramar@hotmail.com>
2025-09-19 20:26:10 -0300

    feat(p1): cria componente principal e faz hello

--- p1/src/App.jsx
+++ p1/src/App.jsx
@@ -4,6 +4,16 @@ class App extends React.Component {
   render() {
     return (
       <>
+        <div className="card">
+          <div className="card-body">
+            <div className="row">
+              <div className="col-12">
+                <p className="text-center fs-4"> Hello Lembretes</p>
+
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     )
   }
